a5a17b609988246d1a2fe35fd205e516523da461
leoguen <leoguenzel@gmail.com>
2022-11-08 14:58:25 +1100

    Finished Extract_Metadata and documented. Also added more papers to the repo.

--- Code/Extract_Metadata/Extract_Metada.py
+++ Code/Extract_Metadata/Extract_Metada.py
@@ -39,40 +39,27 @@ if os.path.exists(str(os.path.join(dir_path, dir_name))) == True:
 else:
     os.mkdir(os.path.join(dir_path, dir_name))
 
-#create another subset only with url and uuid label
-#url_df = ecklonia_df.loc[ecklonia_df[],"point_media_path_best" "label_uuid"]
-
-#download image from webiste and name it according to label.uuid
-
+#save dataframe with only Ecklonia entries in it
+ecklonia_df.to_csv(os.path.join(dir_path, dir_name)+'/Ecklonia_' + csv_list[0] +'.csv', index=False)
 
 for index in ecklonia_df.index:
+    #create name for the cropped image
+    file_name = ecklonia_df.point_media_deployment_campaign_key[index] +"_"+ str(index) +".jpg"
     
-    ###BEGIN TODO###
-    #!!!Problem is label uuid is not refering to image but to label -> all have the same label, maybe work with image name?
-    #Try to save multiple image from dataset and center around point saved in metadata set
-    #Look at Bot Code
-    ###END TODO###
-    file_name = ecklonia_df.label_uuid[index] +".jpg"
-    print(index)
-    print(file_name)
-    print(ecklonia_df.label_uuid[19])
-
     file_path_and_name = os.path.join(dir_path, dir_name, file_name )
+    
     # download the image, convert it to a NumPy array, and then read
 	# it into OpenCV format
     resp = urllib.request.urlopen(ecklonia_df.point_media_path_best[index])
     array_image = np.asarray(bytearray(resp.read()), dtype=np.uint8)
     original_image = cv2.imdecode(array_image, -1) # 'Load it as it is'
     
-    
-    #urllib.request.urlretrieve(ecklonia_df.point_media_path_best[index], file_path_and_name)
-    #original_image= cv2.imread(file_path_and_name)
-    
-    center = original_image.shape
-    x = center[1]/2
-    y = center[0]/2
+    # get dimension of image and multiply with point coordinates
+    x = original_image.shape[1]*ecklonia_df.point_x[index]
+    y = original_image.shape[0]*ecklonia_df.point_y[index]
+    # crop around coordinates
     cropped_image = original_image[int(y-(BOUNDING_BOX_SIZE[0]/2)):int(y+(BOUNDING_BOX_SIZE[0]/2)), int(x-(BOUNDING_BOX_SIZE[1]/2)):int(x+(BOUNDING_BOX_SIZE[1]/2))]
     
+    #save image 
     cv2.imwrite(file_path_and_name, cropped_image)
     print("Saved: " + file_path_and_name)
-    #break
