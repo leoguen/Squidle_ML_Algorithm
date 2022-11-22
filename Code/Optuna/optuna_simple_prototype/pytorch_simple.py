b677daa4b10a9ef123faab05b0e93ca38bcbffa2
leoguen <leoguenzel@gmail.com>
2022-11-22 15:34:45 +1100

    Started to work on smaller PyTorch model. For testing purpose.

--- Code/Optuna/optuna_simple_prototype/pytorch_simple.py
+++ Code/Optuna/optuna_simple_prototype/pytorch_simple.py
@@ -145,7 +145,7 @@ def only_keep_best_trial(best_trial_number, trial_value):
 
 if __name__ == "__main__":
     study = optuna.create_study(direction="maximize")
-    study.optimize(objective, n_trials=2, timeout=600)
+    study.optimize(objective, n_trials=5, timeout=600)
 
     pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
     complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
