11e5e84baf6eac83c4e36d7059b243210ad98305
leoguen <leoguenzel@gmail.com>
2022-12-15 11:20:18 +1100

    Cleaned up .gitignore

--- Code/PyTorch/crop_and_download_images.py
+++ Code/PyTorch/crop_and_download_images.py
@@ -8,29 +8,29 @@ import numpy as np
 
 class crop_download_images():
     
-    def create_directory(HERE, dir_name):    
-        if os.path.exists(HERE+dir_name) == True:
+    def create_directory(save_path, dir_name):    
+        if os.path.exists(save_path+dir_name) == True:
             print("The directory already exist, no new directory is going to be created.")
         else:
-            os.mkdir(HERE+dir_name)
+            os.mkdir(save_path+dir_name)
     
-    def create_directory_structure(self, HERE):
+    def create_directory_structure(self, save_path):
         structure_list = ['/'+str(bounding_box[0])+'_images', '/'+str(bounding_box[0])+'_images/Ecklonia', '/'+str(bounding_box[0])+'_images/Others']
         for i in structure_list:
-            crop_download_images.create_directory(HERE, i)
+            crop_download_images.create_directory(save_path, i)
     
-    def download_images(self, HERE, BOUNDING_BOX, LIST_NAME):
+    def download_images(self, save_path, here,BOUNDING_BOX, LIST_NAME):
         # Used to split up data in Validation and Training
-        csv_file_df= pd.read_csv(HERE + LIST_NAME)
+        csv_file_df= pd.read_csv(here + LIST_NAME)
         csv_file_df.columns = csv_file_df.columns.str.replace('[.]', '_')
         prob_list = []
         for index in range(len(csv_file_df.index)):
             
-            image_path, cropped_image, prob_list = self.crop_image(HERE,csv_file_df, index, BOUNDING_BOX, prob_list)
+            image_path, cropped_image, prob_list = self.crop_image(save_path,csv_file_df, index, BOUNDING_BOX, prob_list)
             #save image 
             try:
                 cv2.imwrite(image_path, cropped_image)
-                print("Saved: " + image_path)
+                print("{} | {} Saved: {}".format(bounding_box[0], index, image_path))
             except:
                 print('Problem with saving index {}'.format(index))
                 prob_list.append([index, csv_file_df.point_media_path_best[index]])
@@ -41,7 +41,7 @@ class crop_download_images():
         df = pd.DataFrame(prob_list)
         df.to_csv('Problematic_files.csv', index=False) 
 
-    def crop_image(self, HERE, csv_file_df, index, bounding_box, prob_list):
+    def crop_image(self, save_path, csv_file_df, index, bounding_box, prob_list):
         #dir_name = '/Training/'
         if csv_file_df.label_name[index] == "Ecklonia radiata":
             label = 'Ecklonia'
@@ -50,7 +50,7 @@ class crop_download_images():
         #create name for the cropped image
         file_name = str(csv_file_df.label_name[index].replace('[.]', '_').replace('(', '_').replace(')', '_').replace(' ', '_')) +"_"+ str(csv_file_df.point_id[index]) +".jpg"
 
-        file_path_and_name = HERE +'/'+str(bounding_box[0])+'_images/' + label + '/' + file_name
+        file_path_and_name = save_path +'/'+str(bounding_box[0])+'_images/' + label + '/' + file_name
         try:
             # download the image, convert it to a NumPy array, and then read
             # it into OpenCV format
@@ -84,12 +84,15 @@ class crop_download_images():
         return file_path_and_name, cropped_image, prob_list
 
 if __name__ == "__main__":
-    bounding_box = [128,128]
-    here = os.path.dirname(os.path.abspath(__file__))
-    save_path = here
-    list_name = '/Annotation_Sets/41250_NORMALIZED_FULL_ANNOTATION_LIST.csv'
+    download_list = [[16,16],[32,32], [64,64], [128,128], [256,256], [512,512]]
+    for bounding_box in download_list:
+        #bounding_box = [128,128]
+        print(bounding_box)
+        here = os.path.dirname(os.path.abspath(__file__))
+        save_path = here
+        list_name = '/Annotation_Sets/41250_NORMALIZED_FULL_ANNOTATION_LIST.csv'
 
-    data = crop_download_images()
+        data = crop_download_images()
 
-    data.create_directory_structure(here)
-    data.download_images(here, bounding_box, list_name)
+        data.create_directory_structure(save_path)
+        data.download_images(save_path, here, bounding_box, list_name)
