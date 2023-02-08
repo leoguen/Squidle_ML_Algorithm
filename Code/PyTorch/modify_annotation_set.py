3bc10904cdb553b3dc1a2ce86cd411d540343088
leoguen <leoguenzel@gmail.com>
2023-02-08 23:12:12 +0000

    Sibling option works.

--- Code/PyTorch/modify_annotation_set.py
+++ Code/PyTorch/modify_annotation_set.py
@@ -30,6 +30,7 @@ class modify_annotation_set():
     '''
     def __init__(self):
         self.sibling = True
+        self.sib_factor = 0.3
 
     def delete_entries(self, csv_file_df, label, value):
         shape_before = csv_file_df.shape[0]
@@ -136,39 +137,51 @@ class modify_annotation_set():
     def get_norm_csv(self, csv_file_df):
         # Get list with all labels and count of each
         classes_df = csv_file_df.pivot_table(index = ['label_name'], aggfunc ='size')
+        #classes_df = classes_df.index.drop_duplicates(keep='first')
+
         if self.sibling:
+            print('Loading Sibling List')
             sib_list_df = pd.read_csv("/home/ubuntu/IMAS/Code/PyTorch/Annotation_Sets/sibling_list.csv", dtype=str, usecols=['Sibling_name'])
-            # Artificially increase number of sibling entries
-            sib_classes_df = classes_df
-            for label in sib_list_df['Sibling_name']:
-                sib_classes_df.loc[label] = sib_classes_df.loc[label] * 5
+            
+            # List without siblings, but Ecklonia 
+            no_sib_classes_df = classes_df.drop(sib_list_df['Sibling_name'])
+            # List with only siblings
+            only_sib_classes_df = classes_df.loc[sib_list_df['Sibling_name']]
 
-            #!!!!!!!!!!!!!!
-            # Find better way to increase siblings
-            #!!!!!!!!!!!!!!
+            # Normalize list without siblings and reduce to percentage defined by self.sib_factor
+            norm_classes_df = no_sib_classes_df.div(no_sib_classes_df.drop('Ecklonia radiata').sum()/(1-self.sib_factor))
+            
+            # Normalize list with only siblings and reduce to percentage defined by self.sib_factor
+            only_sib_classes_df = only_sib_classes_df.div(only_sib_classes_df.sum()/(self.sib_factor))
+            
+            # Add both lists
+            norm_classes_df = norm_classes_df.append(only_sib_classes_df)
 
-
-
-            # Normalize all classes so that the overall number is equal to Ecklonia entries, number slightly increazed by 0.95 because of download errors in later process
-            norm_classes_df = sib_classes_df.div(classes_df.drop('Ecklonia radiata').sum()*0.95)
         else:
-            # Normalize all classes so that the overall number is equal to Ecklonia entries, number slightly increazed by 0.95 because of download errors in later process
-            norm_classes_df = classes_df.div(classes_df.drop('Ecklonia radiata').sum()*0.95)
+            # Normalize all classes so that the overall number is equal to Ecklonia entries
+            norm_classes_df = classes_df.div(classes_df.drop('Ecklonia radiata').sum())
         
         norm_classes_df = (norm_classes_df.mul(classes_df['Ecklonia radiata'])).astype(int)
 
         # Correct Ecklonia number to all entries
         norm_classes_df['Ecklonia radiata'] = classes_df['Ecklonia radiata']
+
+        if self.sibling:
+            print('Dataset comprises of {} Sibling Entries and {} out of {} total entries'.format(
+                norm_classes_df.loc[sib_list_df['Sibling_name']].sum(), 
+                norm_classes_df.loc['Ecklonia radiata'], 
+                norm_classes_df.sum()))
         return norm_classes_df, classes_df
     
     def normalize_set(self, csv_file_df):
         norm_classes_df, classes_df = self.get_norm_csv(csv_file_df)
+        norm_classes_df = norm_classes_df[~norm_classes_df.index.duplicated(keep='first')]
         csv_file_df = csv_file_df.reset_index()
         np.random.seed(10)
         print('Normalizing Entries')
         counter = 0
         for label, amount in norm_classes_df.items():
-            print('Processed {} out of {}'.format(counter, len(norm_classes_df)), end='\r')
+            print('Processed {} out of {}: {}'.format(counter, len(norm_classes_df), label), end='\r')
             remove_n = classes_df[label] - amount
             csv_except_df = csv_file_df.loc[csv_file_df['label_name'] == label]
             drop_indices = np.random.choice(csv_except_df.index, remove_n, replace=False)
@@ -189,7 +202,7 @@ class modify_annotation_set():
     def save_csv(self, csv_file_df, description):
         
         if self.sibling:
-            path =here+'/Annotation_Sets/'+ str(len(csv_file_df.index))+'_sibling_'+ description +'_list.csv'
+            path =here+'/Annotation_Sets/'+ str(len(csv_file_df.index))+'_' + str(int(self.sib_factor*100)) +'_sibling_'+ description +'_list.csv'
         else: 
             path = here+'/Annotation_Sets/'+ str(len(csv_file_df.index))+'_'+ description +'_list.csv'
         
