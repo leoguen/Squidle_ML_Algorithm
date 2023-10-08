9683987b492d61a75d0448ba3bfee3c848321e2b
leoguen <leoguenzel@gmail.com>
2023-10-08 15:36:51 +0000

    Added Lineage

--- Code/PyTorch/modify_annotation_set.py
+++ Code/PyTorch/modify_annotation_set.py
@@ -1,4 +1,4 @@
-
+import re
 import os
 from os import path
 import pandas as pd
@@ -8,13 +8,34 @@ class modify_annotation_set():
 
     def __init__(self):
         self.sibling = False
+        self.sibling_list_path = "/home/ubuntu/IMAS/Code/PyTorch/Annotation_Sets/sibling_list.csv"
         self.red_list = False
         self.neighbour = True
         self.sib_factor = 0.3
-        self.coi = 'Seagrass cover' #Class of Interest
-        
-        self.row_name = 'label_translated_name' #'label_name'  
+        #self.lineage = "Physical > Substrate > Unconsolidated (soft) > Sand / mud (<2mm)"
+        self.coi = 'Physical > Substrate > Unconsolidated (soft) > Sand / mud (<2mm)' #Class of Interest
+        self.defined_name = "" # The resulting csv will always be saved with the lineage name, this string will be added adter the lineage name
+        self.col_name = 'label_translated_lineage_names' #'label_name'  
         self.norm_factor = 1
+        self.save_path = "/pvol/Annotationsets/Full_Annotationsets/"
+
+    def replace_lineage(self, csv_file_df):
+        """
+        Finds lineages that include the target lineage but also go more into depth and replace that with the lineage depth that is required
+        """
+        count = (csv_file_df[self.col_name] == self.coi).sum()
+        print("Number of {} Entries: {} After replace_lineage".format(self.coi, count))
+        # Create a boolean mask to identify rows that contain the substring
+        mask = csv_file_df[self.col_name].str.contains(self.coi, case=False, na=False, regex=False)
+
+        # Replace the values in the specified column with self.coi where the mask is True
+        csv_file_df.loc[mask, self.col_name] = self.coi
+
+        count = (csv_file_df[self.col_name] == self.coi).sum()
+        print("Number of {} Entries: {} After replace_lineage".format(self.coi, count))
+
+        return csv_file_df
+
 
     def delete_entries(self, csv_file_df, label, value):
         shape_before = csv_file_df.shape[0]
@@ -26,27 +47,27 @@ class modify_annotation_set():
         shape_before = csv_file_df.shape[0]
         og_shape =shape_before
         if self.red_list:
-            red_list_df = pd.read_csv("/home/ubuntu/Documents/IMAS/Code/PyTorch/Annotation_Sets/red_list.csv", dtype=str, usecols=[self.row_name])
-            for value in red_list_df[self.row_name]:
-                csv_file_df = self.delete_entries(csv_file_df, self.row_name, value)
+            red_list_df = pd.read_csv("/home/ubuntu/Documents/IMAS/Code/PyTorch/Annotation_Sets/red_list.csv", dtype=str, usecols=[self.col_name])
+            for value in red_list_df[self.col_name]:
+                csv_file_df = self.delete_entries(csv_file_df, self.col_name, value)
 
         csv_file_df = self.delete_entries(csv_file_df, 'tag_names', 'Flagged For Review')
         #print('Not being saved to reduce space.')
         # Update the index after deleting entries 
         csv_file_df.reset_index(drop=True, inplace=True)
-        self.save_csv(csv_file_df, 'review')
+        #self.save_csv(csv_file_df, 'review')
         return csv_file_df
 
     def get_norm_csv(self, csv_file_df):
         # Get list with all labels and count of each
-        classes_df = csv_file_df.pivot_table(index = [self.row_name], aggfunc ='size')
+        classes_df = csv_file_df.pivot_table(index = [self.col_name], aggfunc ='size')
         #classes_df = classes_df.index.drop_duplicates(keep='first')
         if self.neighbour:
             print('self.neighbour is set to True')
             # Get all COI rows in csv_file_df
-            only_coi_df = csv_file_df.loc[csv_file_df[self.row_name] == self.coi]
+            only_coi_df = csv_file_df.loc[csv_file_df[self.col_name] == self.coi]
 
-            print('Number of Entries {} in relation to {} Images'.format(len(csv_file_df['label_name']),len(csv_file_df.drop_duplicates(subset=['point_media_path_best'])['label_name'])))
+            print('Number of Entries {} in relation to {} Images'.format(len(csv_file_df[self.col_name]),len(csv_file_df.drop_duplicates(subset=['point_media_path_best'])[self.col_name])))
 
             print('Number of COI entries {}'.format(len(only_coi_df)))
             # Get list of image paths without duplicates
@@ -58,10 +79,10 @@ class modify_annotation_set():
             print('Number of COI Image related entries {}'.format(len(addi_anno_coi)))
 
             # Drop the class of interest
-            addi_anno_coi = addi_anno_coi.drop(addi_anno_coi[addi_anno_coi[self.row_name] == self.coi].index)
+            addi_anno_coi = addi_anno_coi.drop(addi_anno_coi[addi_anno_coi[self.col_name] == self.coi].index)
             print('Number of COI Image related entries without COI entries {}'.format(len(addi_anno_coi)))
             # Get all the labels and amount of entries per label in these additional annotations
-            labels_addi = addi_anno_coi.pivot_table(index = [self.row_name], aggfunc ='size')
+            labels_addi = addi_anno_coi.pivot_table(index = [self.col_name], aggfunc ='size')
             
             # Normalize all classes so that the overall number is equal to Ecklonia entries
             norm_classes_df = classes_df.div(classes_df.drop(self.coi).sum())
@@ -80,7 +101,7 @@ class modify_annotation_set():
             
         if self.sibling:
             print('Loading Sibling List')
-            sib_list_df = pd.read_csv("/home/ubuntu/IMAS/Code/PyTorch/Annotation_Sets/sibling_list.csv", dtype=str, usecols=['Sibling_name'])
+            sib_list_df = pd.read_csv(self.sibling_list_path, dtype=str, usecols=['Sibling_name'])
             
             # List without siblings, but Ecklonia 
             no_sib_classes_df = classes_df.drop(sib_list_df['Sibling_name'])
@@ -115,20 +136,21 @@ class modify_annotation_set():
         return norm_classes_df, classes_df
     
     def normalize_set(self, csv_file_df):
-        csv_file_df = self.delete_review(csv_file_df)
+
         if self.neighbour: 
             norm_classes_df, classes_df, addi_anno_coi = self.get_norm_csv(csv_file_df)
             # drop the additional annotations from csv_file_df
             cond = csv_file_df['point_id'].isin(addi_anno_coi['point_id'])
             csv_file_df = csv_file_df.drop(csv_file_df[cond].index)
             # Get the classes from the residual testset
-            classes_df = csv_file_df.pivot_table(index = [self.row_name], aggfunc ='size')
+            classes_df = csv_file_df.pivot_table(index = [self.col_name], aggfunc ='size')
             # Throw out all norm_calsses_df entries that are not in the excpet anymore
             cond = ~norm_classes_df.index.isin(classes_df.index)
             norm_classes_df = norm_classes_df.drop(norm_classes_df[cond].index)
 
         else:
             norm_classes_df, classes_df = self.get_norm_csv(csv_file_df)
+        
         norm_classes_df = norm_classes_df[~norm_classes_df.index.duplicated(keep='first')]
         csv_file_df = csv_file_df.reset_index()
         np.random.seed(10)
@@ -140,30 +162,26 @@ class modify_annotation_set():
             remove_n = classes_df[label] - int(amount)
             if remove_n < 0:
                 remove_n = 0
-            csv_except_df = csv_file_df.loc[csv_file_df[self.row_name] == label]
+            csv_except_df = csv_file_df.loc[csv_file_df[self.col_name] == label]
             drop_indices = np.random.choice(csv_except_df.index, remove_n, replace=False)
             csv_file_df = csv_file_df.drop(drop_indices)
             counter +=1
-            '''
-            try:
-                print('{} entries: {}'.format(label, csv_file_df.pivot_table(index = ['label_name'], aggfunc ='size')[label]))
-            except:
-                print('{} all {} entries deleted'.format(label, remove_n))
-            '''
+
         if self.neighbour:
             #add addi_anno_coi to csv_file_df
             print('')
             print(len(csv_file_df))
-            print(len(csv_file_df[csv_file_df[self.row_name] == self.coi]))
+            print(len(csv_file_df[csv_file_df[self.col_name] == self.coi]))
             print(len(addi_anno_coi))
             csv_file_df = csv_file_df.append(addi_anno_coi)
             print(len(csv_file_df)) 
             csv_file_df = csv_file_df.drop_duplicates()
             print(len(csv_file_df))
-        print('Normalized Dataset size: {} with {} classes'.format(csv_file_df.pivot_table(index = [self.row_name], aggfunc ='size').sum(), len(csv_file_df.pivot_table(index = [self.row_name], aggfunc ='size'))))
+        print('Normalized Dataset size: {} with {} classes'.format(csv_file_df.pivot_table(index = [self.col_name], aggfunc ='size').sum(), len(csv_file_df.pivot_table(index = [self.col_name], aggfunc ='size'))))
         csv_file_df.reset_index(drop=True, inplace=True)
         #csv_name = self.coi + '_NMSC'
-        self.save_csv(csv_file_df, self.coi.replace(' ', '_')  + '_NMSC')
+        file_name = self.clean_and_extract_filename(self.coi)
+        self.save_csv(csv_file_df, file_name  + self.defined_name)
         return csv_file_df
 
     def save_csv(self, csv_file_df, description):
@@ -171,35 +189,54 @@ class modify_annotation_set():
             description = description + '_red_listed'
         
         if self.sibling:
-            path =here+'/Annotation_Sets/'+ str(len(csv_file_df.index))+'_' + str(int(self.sib_factor*100)) +'_sibling_'+ description +'_list.csv'
+            path = self.save_path+ str(len(csv_file_df.index))+'_' + str(int(self.sib_factor*100)) +'_sibling_'+ description +'_list.csv'
         elif self.norm_factor != 1:
-            path =here+'/Annotation_Sets/'+ str(len(csv_file_df.index))+'_1_to_' + str(int(self.norm_factor)) + description +'_list.csv'
+            path =self.save_path + str(len(csv_file_df.index))+'_1_to_' + str(int(self.norm_factor)) + description +'_list.csv'
         elif self.neighbour:
-            path =here+'/Annotation_Sets/'+ str(len(csv_file_df.index))+'_neighbour_' + description +'_list.csv'
+            path = self.save_path + str(len(csv_file_df.index))+'_neighbour_' + description +'_list.csv'
         else: 
-            path = here+'/Annotation_Sets/'+ str(len(csv_file_df.index))+'_'+ description +'_list.csv'
+            path = self.save_path + str(len(csv_file_df.index))+'_'+ description +'_list.csv'
         
         csv_file_df.to_csv(path,index=False)
         print('Saved {} entries with filename: {}'.format(len(csv_file_df.index), path))
 
+    def clean_and_extract_filename(self, input_string):
+        # Split the input string by ">"
+        parts = input_string.split(">")
+        
+        # Take the last part of the split as the filename
+        filename = parts[-1].strip()
+        
+        # Remove special characters that are not allowed in filenames
+        cleaned_filename = re.sub(r'[\/:*?"<>|]', '_', filename)
+        
+        return cleaned_filename
     
 if __name__ == "__main__":
-    pd.set_option("display.max_rows", None, "display.max_columns", None)
+    pd.set_option("display.max_rows", 10, "display.max_columns", 10)
     
-    here = os.path.dirname(os.path.abspath(__file__))
     print('Loading CSV file, this may take a while.')
-    list_name = '/Annotation_Sets/Final_Sets/731_Full_Annotation_List_NMSC_Seagrass_modified.csv'
-    csv_file_df= pd.read_csv(here + list_name, on_bad_lines='skip', low_memory=False) 
+    
+    list_name = '/pvol/Annotationsets/Full_Annotationsets/20231002_803_Full_Annotation_List.csv'
+    
+    csv_file_df= pd.read_csv(list_name, on_bad_lines='skip', low_memory=False) 
     
     print('Loaded {} entries with filename: {}'.format(len(csv_file_df.index), list_name))
 
     csv_file_df.columns = csv_file_df.columns.str.replace('[.]', '_', regex=True) #replace dots with underscores
 
     # Used for NMSC dataset
-    values = {"label_translated_name": 'Not of Interest'}
-    csv_file_df = csv_file_df.fillna(value=values)
+    #values = {"label_translated_name": 'Not of Interest'}
+    #csv_file_df = csv_file_df.fillna(value=values)
 
     data = modify_annotation_set()
 
+    # Deletes entries that should not be used (e.g. "Flagged for Review") 
+    csv_file_df = data.delete_review(csv_file_df)
+
+    # Replace the more in depth lineages with the target lineage if required
+    csv_file_df = data.replace_lineage(csv_file_df)
+
     data.normalize_set(csv_file_df)
+    
     
